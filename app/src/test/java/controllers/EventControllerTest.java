8cb9b98d23981fd3a6770f59a63f5bfae83b6e23
Szymon Garbień <szymongarbien@gmail.com>
2023-02-17 14:26:30 +0100

    More unit tests

--- app/src/test/java/controllers/EventControllerTest.java
+++ app/src/test/java/controllers/EventControllerTest.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import org.example.controllers.EventController;
+import org.example.converters.EventConverter;
 import org.example.model.CompetitorModel;
 import org.example.model.EventModel;
 import org.example.model.InlineObject;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class EventControllerTest {
@@ -35,25 +37,67 @@ class EventControllerTest {
     CompetitorRepository competitorRepository;
 
     @Test
-    void addEvents_validDataGiven_validResponseReturned() {
+    void addEvents_validDataGiven_repositoryWrittenValidResponseReturned() {
         //given
-        List<CompetitorModel> competitors = new ArrayList<>();
-        competitors.add(new CompetitorModel().id("sr:competitor:37863")
-                .name("SS Folgore Falciano Calcio")
-                .country("San Marino")
-                .countryCode("SMR")
-                .abbreviation("FFC")
-                .qualifier("home")
-                .gender("male"));
-        competitors.add(new CompetitorModel().id("sr:competitor:277829")
-                .name("FC Prishtina")
-                .country("Kosovo")
-                .countryCode("KOS")
-                .abbreviation("PRI")
-                .qualifier("away")
-                .gender("male"));
+        InlineObject data = new InlineObject();
+        data.addEventsItem(getTestEventModel());
+        //when
+        ResponseEntity<Void> response = controller.addEvents(data);
+        //then
+        assertEquals(new ResponseEntity<>(HttpStatus.CREATED), response);
+        verify(eventRepository).save(EventConverter.convertModel(getTestEventModel()));
+    }
 
-        EventModel eventModel = new EventModel()
+    @Test
+    void addEvents_invalidDataGiven_exceptionThrown() {
+        //given
+        InlineObject data = new InlineObject();
+        data.addEventsItem(new EventModel());
+        //when
+        //then
+        assertThrows(NullPointerException.class, () -> controller.addEvents(data));
+    }
+
+    /*
+     * getEvents method accepts either 15, 20 or 100 query number
+     */
+    @Test
+    void getEvents_validRequestEmptyRepository_emptyResponseBodyStatusOKReturned() {
+        //given
+        ResponseEntity<String> expected =
+                new ResponseEntity<>("15 most probable results:\n\n",HttpStatus.OK);
+        //when
+        ResponseEntity<String> response = controller.getEvents(15);
+        //then
+        assertEquals(expected, response);
+    }
+
+    @Test
+    void getEvents_invalidRequest_responseWithBadRequestStatusReturned() {
+        //given
+        ResponseEntity<String> expected = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        //when
+        ResponseEntity<String> response = controller.getEvents(10);
+        //then
+        assertEquals(expected, response);
+    }
+
+    @Test
+    void getTeams_emptyRepository_emptyResponseReturned() {
+        //given
+        ResponseEntity<String> expected = new ResponseEntity<>("Team names:\n\n", HttpStatus.OK);
+        //when
+        ResponseEntity<String> response = controller.getTeams();
+        //then
+        assertEquals(expected, response);
+    }
+
+    EventModel getTestEventModel() {
+        List<CompetitorModel> competitors = new ArrayList<>();
+        competitors.add(getTestCompetitorModel1());
+        competitors.add(getTestCompetitorModel2());
+
+        return new EventModel()
                 .sportEventId("sr:sport_event:27636100")
                 .startDate(OffsetDateTime.parse("2021-06-22T18:00:00+00:00"))
                 .sportName("Soccer")
@@ -71,22 +115,25 @@ class EventControllerTest {
                 .probabilityHomeTeamWinner(BigDecimal.valueOf(2.5))
                 .probabilityDraw(BigDecimal.valueOf(88.1))
                 .probabilityAwayTeamWinner(BigDecimal.valueOf(9.4));
-
-        InlineObject data = new InlineObject();
-        data.addEventsItem(eventModel);
-        //when
-        ResponseEntity<Void> response = controller.addEvents(data);
-        //then
-        assertEquals(new ResponseEntity<>(HttpStatus.CREATED), response);
     }
 
-    @Test
-    void addEvents_invalidDataGiven_exceptionThrown() {
-        //given
-        InlineObject data = new InlineObject();
-        data.addEventsItem(new EventModel());
-        //when
-        //then
-        assertThrows(NullPointerException.class, () -> controller.addEvents(data));
+    CompetitorModel getTestCompetitorModel1() {
+        return new CompetitorModel().id("sr:competitor:37863")
+                .name("SS Folgore Falciano Calcio")
+                .country("San Marino")
+                .countryCode("SMR")
+                .abbreviation("FFC")
+                .qualifier("home")
+                .gender("male");
+    }
+
+    CompetitorModel getTestCompetitorModel2() {
+        return new CompetitorModel().id("sr:competitor:277829")
+                .name("FC Prishtina")
+                .country("Kosovo")
+                .countryCode("KOS")
+                .abbreviation("PRI")
+                .qualifier("away")
+                .gender("male");
     }
 }
